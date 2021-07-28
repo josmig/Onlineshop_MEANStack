e53bfd47d15c3ad19f49eb446c70e56a35fba7d2
josmig <pantigosomiguel59@gmail.com>
2021-07-28 00:25:49 -0500

    creando la estructura de componentes para el dashboard admin

---
+++ src/app/@admin/core/components/title/title.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-title',
+  templateUrl: './title.component.html',
+  styleUrls: ['./title.component.css']
+})
+export class TitleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
