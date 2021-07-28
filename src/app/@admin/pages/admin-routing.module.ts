65311578f1ae57f33a7faebc70c9ca77d873a02e
josmig <pantigosomiguel59@gmail.com>
2021-07-28 00:11:05 -0500

    integrando template basico - bootstrap /parte administrador

--- src/app/@admin/pages/admin-routing.module.ts
+++ src/app/@admin/pages/admin-routing.module.ts
@@ -1,6 +1,5 @@
 import { AdminComponent } from './admin.component';
-import { UsersComponent } from './users/users.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
+
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
